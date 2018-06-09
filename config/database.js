9cea049ab789c060a431e6e758d64662235955d1
nabiypy <nabiypy@gmail.com>
2018-06-09 19:27:51 +0000

    init cofiglobal

---
+++ config/database.js
@@ -0,0 +1,5 @@
+if(process.env.NODE_ENV === 'production'){
+  module.exports = {mongoURI: 'mongodb://sasghana:digimas14@ds153890.mlab.com:53890/cofiglobal'}
+} else {
+  module.exports = {mongoURI: 'mongodb://sasghana:digimas14@ds153890.mlab.com:53890/cofiglobal'}
+}
