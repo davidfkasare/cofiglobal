c4a1a07a72b6c27dc39c9739b4b0fc9a47a3a36f
nabiypy <nabiypy@gmail.com>
2018-06-09 19:35:41 +0000

    changed port to 8080

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.get('/about', (req, res) => {
 app.use('/ideas', ideas);
 app.use('/users', users);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () =>{
   console.log(`Server started on port ${port}`);
